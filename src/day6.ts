ccabe8b7e59ff0289bf8ef499f6f7fd3dc83d0e6
Tomas Pavlatka <tomas@pavlatka.cz>
2025-12-06 14:01:58 +0100

    feat: working solution

--- src/day6.ts
+++ src/day6.ts
@@ -5,25 +5,53 @@ export const part1 = (sample: boolean): number => {
   const worksheet = getWorksheet(1, sample);
 
   let total = 0;
-  for (let col = 0; col < worksheet.cols; col++) {
+  for (let col = 0; col < worksheet.indexes.length; col++) {
     const colOperation = worksheet.operations.get(col) || '+';
     if (colOperation == '+') {
-      total += [...Array(worksheet.rows).keys()]
+      total += [...Array(worksheet.rows + 1).keys()]
         .map(row => worksheet.numbers.get(toStringCoord(row, col)) || 0)
       .reduce((a, b) => a += b, 0);
     } else {
-      total +=[...Array(worksheet.rows).keys()]
+      total += [...Array(worksheet.rows + 1).keys()]
         .map(row => worksheet.numbers.get(toStringCoord(row, col)) || 0)
       .reduce((a, b) => a *= b, 1);
     }
   }
 
   return total;
-
 }
 
 export const part2 = (sample: boolean): number => {
-  return 0;
+  const worksheet = getWorksheet(2, sample);
+
+  let total = 0;
+  for (let i = 0; i < worksheet.indexes.length; i++) {
+    const start = worksheet.indexes[i];
+    const end = i < worksheet.indexes.length - 1 ? worksheet.indexes[i+1] - 2 : worksheet.cols;
+
+    const reals = [];
+    for (let col = start; col <= end; col++) {
+      const nums: number[] = [];
+      for (let row = 0; row <= worksheet.rows; row++) {
+        const val = worksheet.maze.get(toStringCoord(row, col));
+        if (val != undefined && val != ' ' && val != '+' && val != '*') {
+          nums.push(Number(val));
+        }
+      }
+      reals.push(nums);
+    }
+
+    const d = reals.map(nums => Number(nums.join('')));
+
+    const operator = worksheet.operations.get(i);
+    if (operator == '+') {
+      total += d.reduce((a,b) => a += b, 0);
+    } else {
+      total += d.reduce((a,b) => a *= b, 1);
+    }
+  }
+
+  return total;
 }
 
 const getWorksheet = (part: number, sample: boolean): Worksheet => {
@@ -31,6 +59,8 @@ const getWorksheet = (part: number, sample: boolean): Worksheet => {
 
   const numbers: Map<string, number> = new Map();
   const operations: Map<number, string> = new Map();
+  const maze: Map<string, string> = new Map();
+  const indexes = [];
 
   let cols = 0;
   for (let row = 0; row < lines.length; row++) {
@@ -42,21 +72,34 @@ const getWorksheet = (part: number, sample: boolean): Worksheet => {
         numbers.set(toStringCoord(row, col), nums[col]);
       }
 
-      cols = nums.length;
+      cols = line.length - 1;
     } else {
-      const oprs = [...line.matchAll(/[\+,\*]/g)];
+      const oprs = line.split('');
+      let oprIdx = 0;
       for (let col = 0; col < oprs.length; col++) {
-        operations.set(col, oprs[col].toString());
+        const colOpr = oprs[col];
+        if (colOpr == '+' || colOpr == '*') {
+          operations.set(oprIdx++, colOpr);
+          indexes.push(col);
+        }
+      }
+    }
+
+    for (let col = 0; col < line.length; col++) {
+      if (line.match(/\d+/)) {
+        maze.set(toStringCoord(row, col), line[col]);
       }
     }
   }
 
-  return { numbers, operations, rows: lines.length - 1, cols }
+  return { numbers, operations, indexes, rows: lines.length - 2, cols, maze }
 }
 
 interface Worksheet {
   numbers: Map<string, number>,
+  maze: Map<string, string>,
   operations: Map<number, string>
+  indexes: number[],
   rows: number
   cols: number
 }
--- tests/day6.spec.ts
+++ tests/day6.spec.ts
@@ -1,19 +1,19 @@
 import { part1, part2 } from "@src/day6";
 
 describe('day6', () => {
-  it.only('part 1, sample', () => {
+  it('part 1, sample', () => {
     expect(part1(true)).toBe(4277556);
   });
 
-  it.only('part 1', () => {
+  it('part 1', () => {
     expect(part1(false)).toBe(4693419406682);
   });
 
   it('part 2, sample', () => {
-    expect(part2(true)).toBe(6);
+    expect(part2(true)).toBe(3263827);
   });
 
   it('part 2', () => {
-    expect(part2(false)).toBe(5892);
+    expect(part2(false)).toBe(9029931401920);
   });
 });
